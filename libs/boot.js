5fd90f1e0174bd38834f99766962a6c64364925b
leandrowylde <leandro.job@live.com>
2020-02-14 15:56:53 -0300

    Update of init boot to test app

--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,9 @@
 module.exports = app => {
-  app.db.sequelize.sync().done(() => {
-    app.listen(app.get("port"), () => {
-      console.log(`API Started at http://localhost:${app.get("port")}`);
+  if (process.env.NODE_ENV !== "test") {
+    app.db.sequelize.sync().done(() => {
+      app.listen(app.get("port"), () => {
+        console.log(`API Started at port :${app.get("port")}`);
+      });
     });
-  });
+  }
 };
